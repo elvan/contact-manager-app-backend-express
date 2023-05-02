6010d2460f02d99870663f06002eace67982a5ee
Elvan Hidayat <elvanhd@gmail.com>
2023-05-02 07:07:32 +0700

    connect to mongodb

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
