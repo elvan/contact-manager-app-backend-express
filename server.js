6f96813860492c09ee12a106b94e7fb62738c008
Elvan Hidayat <elvanhd@gmail.com>
2023-05-02 07:36:10 +0700

    register route

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json());
+
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the ContactManager API' })
 );
